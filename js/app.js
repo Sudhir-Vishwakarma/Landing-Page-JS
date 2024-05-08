2987620cbcc65c3a6772a3a0a41f4f10d3ddc8d5
Sudhir Vishwakarma <karmasudhir48@gmail.com>
2024-05-08 13:28:13 +0530

    Second Commit Landing Page

--- js/app.js
+++ js/app.js
@@ -11,3 +11,59 @@ closeSidebar.addEventListener("click", ()=>{
 openSidebar.addEventListener("click", ()=>{
     sideBarOpen.style.display = "block";
 });
+
+
+// Signup //
+const sideBarSignUp = document.getElementById("signup-id");
+const openSignup = document.querySelector(".nav-signup-btn");
+const closeSignUp = document.querySelector(".signup-mark");
+const signupclose = document.querySelector(".signup-bg");
+
+
+closeSignUp.addEventListener("click", ()=>{
+    signupclose.style.display= "none";
+});
+openSignup.addEventListener("click", ()=>{
+    signupclose.style.display = "block";
+ })
+ sideBarSignUp.addEventListener("click", ()=>{
+    signupclose.style.display = "block";
+    sideBarClose.style.display = "none";
+ })
+
+
+// Login //
+const loginOpen = document.getElementById("login-id");
+const loginClose = document.querySelector(".login-xmark");
+const closeLogin = document.querySelector(".login-bg");
+const openLogin = document.querySelector(".nav-login-btn");
+
+loginClose.addEventListener("click", ()=>{  
+    closeLogin.style.display = "none";
+});
+
+openLogin.addEventListener("click", ()=>{
+    closeLogin.style.display = "block";
+});
+
+loginOpen.addEventListener("click", ()=>{
+    closeLogin.style.display = "block";
+    sideBarClose.style.display = "none";
+});
+
+
+// Login to Register and Register to login //
+const registerToLogin = document.querySelector(".signup-login-btn");
+
+registerToLogin.addEventListener("click", ()=>{
+    closeLogin.style.display = "block";
+    signupclose.style.display = "none";
+    console.log("Button Clicked");
+});
+
+const loginToRegister = document.querySelector(".login-signup-btn");
+
+loginToRegister.addEventListener("click", ()=>{
+    signupclose.style.display = "block";
+    closeLogin.style.display = "none";
+})
